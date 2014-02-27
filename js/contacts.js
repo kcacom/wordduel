80568bc8b54b096d788e5a3a018e99668e3a2fce
kcacom <kcutler@ancestry.com>
2014-02-27 14:53:25 -0700

    Adding support in for compose email

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,6 @@
+
+var MY_DEVICE_REG_ID = "myDeviceRegId";
+
 var app = {
 
 	notify: function (message, title) {
@@ -39,6 +42,30 @@ var app = {
 				{"name":{"givenName":"Joseph","familyName":"Morrow"},"emails":[{"type":"home","value":"joe@themorrowgroup.com","pref":true},{"type":"home","value":"joseph@themorrowgroup.com","pref":false}]}
 			];
 			self.render(contacts);
+		}
+		
+		if (window.addEventListener) {
+			window.addEventListener("storage", self.storageHandler, false);
+		} else {
+			window.attachEvent("onstorage", self.storageHandler);
+		}
+	},
+	
+	storageHandler: function(evt) {
+		
+		evt = evt || window.event;
+		// The event object will contain the following attributes:
+		evt.key; // The key of the modified key-value pair.
+		evt.oldValue; // Previous value of the pair (null for a new pair).
+		evt.newValue; // New value for the pair (null for a deleted pair).
+		evt.url; // URL from which the change was made.
+		// Note: might be named event.uri instead in old browsers.
+		
+		if (evt.key == MY_DEVICE_REG_ID) {
+			var deviceRegId = window.localStorage.getItem(MY_DEVICE_REG_ID);
+			if (deviceRegId) {
+				// Update the UI to show that we're able to send our deviceRegId out
+			}
 		}
 	}
 };
