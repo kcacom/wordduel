a80b522f85c02d1149d1d5f83a0e033054831c52
Patrick Morrow <git@themorrowgroup.com>
2014-02-27 12:18:11 -0700

    display the contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,18 +1,29 @@
 var app = {
 
-	onSuccess: function (){
-		alert('onSuccess');
-	}
+	showAlert: function (message, title) {
+	    if (navigator.notification) {
+	        navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+	        alert(title ? (title + ": " + message) : message);
+	    }
+	},
 
-	onError: function (){
-		alert('onError');
-	}
+	onSuccess: function () {
+		showAlert('onSuccess', 'Info');
+		for (var i=0; i<contacts.length; i++) {
+			$('#contacts').append('<div>' + contacts[i].firstName + ' ' + contacts[i].firstName + '</div><div>' +  contacts[i].email + '</div>');
+		}
+	},
+
+	onError: function () {
+		showAlert('onError', 'Info');
+	},
 
 	initialize: function() {
         var options = new ContactFindOptions();
         options.filter = '';
         options.multiple = true;
-        var filter = ['firstName', 'email'];
+        var filter = ['firstName', 'lastName', 'email'];
 
         navigator.contacts.find(filter, onSuccess, onError, options);
 	}
