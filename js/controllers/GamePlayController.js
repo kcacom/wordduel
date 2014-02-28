6931407ca9e39e37309491a0a7610751bc4194db
Bressain Dinkelman <bdinkelman@ancestry.com>
2014-02-27 22:22:41 -0700

    Loading new game state.

--- js/controllers/GamePlayController.js
+++ js/controllers/GamePlayController.js
@@ -4,10 +4,15 @@ wordDuel.controller('GamePlayCtrl', function GamePlayCtrl($scope, gamePlayStorag
 	var LETTER_POSSIBLE_STATE = 'p';
 	var LETTER_IMPOSSIBLE_STATE = 'i';
 	var LETTER_DEFINITE_STATE = 'd';
+	var GAME_CHOOSE_WORD_STATE = 'choose-word';
+	var GAME_PLAYING_STATE = 'playing';
 
 	$scope.name = gamePlayStorage.getMyName();
 	$scope.opponentName = loadOpponentInfo(urlParams.opponent);
 	$scope.game = loadGameState(urlParams.opponent);
+
+	var state = getGameState($scope.game);
+	$scope.buttonText = getButtonText(state);
 
 	// listen for external game state changes
 	$('#gamePlayBus').bind('successfulStateCallback', function() {
@@ -29,40 +34,29 @@ wordDuel.controller('GamePlayCtrl', function GamePlayCtrl($scope, gamePlayStorag
 	function loadGameState(opponent) {
 		var current = $scope.game;
 		var game = gamePlayStorage.getGameState(opponent);
+
+		if (game.opponent === undefined) return createNewGame(opponent);
+
 		if (current !== undefined) {
 			game.letterStates = current.letterStates;
 		} else if (game.letterStates === undefined) {
-			game.letterStates = createLetterStates(game);
+			game.letterStates = createLetterStates();
 		}
 		game.roundsData = getRoundsExtraData(game);
 		return game;
 	}
 
-	function getRoundsExtraData(game){
-		var roundsData = [];
-		for (var i=0; i<game.rounds.length; i+=1) {
-			roundsData[i] = {
-				yours: {
-					parts: game.rounds[i].yours.split(''),
-					matches: getMatches(game.rounds[i].yours, game.theirWord)
-				},
-				theirs: {
-					parts: game.rounds[i].theirs.split(''),
-					matches: getMatches(game.rounds[i].theirs, game.yourWord)
-				}
-			}
+	function createNewGame(opponent) {
+		return {
+			opponent: opponent,
+			yourWord: '',
+			theirWord: '',
+			rounds: [],
+			letterStates: createLetterStates()
 		}
-		return roundsData;
 	}
 
-	 function getMatches(guess, word) {
-		var matches = guess.match(new RegExp('[' + word + ']', 'gi'));
-		if (matches === null)
-			return 0;
-		return matches.length;
-	}
-
-	function createLetterStates(game) {
+	function createLetterStates() {
 		return {
 			a: LETTER_POSSIBLE_STATE,
 			b: LETTER_POSSIBLE_STATE,
@@ -91,5 +85,41 @@ wordDuel.controller('GamePlayCtrl', function GamePlayCtrl($scope, gamePlayStorag
 			y: LETTER_POSSIBLE_STATE,
 			z: LETTER_POSSIBLE_STATE
 		};
+	}
+
+	function getRoundsExtraData(game){
+		var roundsData = [];
+		for (var i=0; i<game.rounds.length; i+=1) {
+			roundsData[i] = {
+				yours: {
+					parts: game.rounds[i].yours.split(''),
+					matches: getMatches(game.rounds[i].yours, game.theirWord)
+				},
+				theirs: {
+					parts: game.rounds[i].theirs.split(''),
+					matches: getMatches(game.rounds[i].theirs, game.yourWord)
+				}
+			}
+		}
+		return roundsData;
+	}
+
+	function getMatches(guess, word) {
+		var matches = guess.match(new RegExp('[' + word + ']', 'gi'));
+		if (matches === null)
+			return 0;
+		return matches.length;
+	}
+
+	function getGameState(game) {
+		if (game.yourWord === undefined || game.yourWord === '' || game.theirWord === undefined || game.theirWord === '')
+			return GAME_CHOOSE_WORD_STATE;
+		return GAME_PLAYING_STATE;
+	}
+
+	function getButtonText(state) {
+		if (state === GAME_CHOOSE_WORD_STATE)
+			return "Choose Secret Word";
+		return "Guess";
 	}
 });
