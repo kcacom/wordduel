969e87fd4b12b17072822e48a1295dbff8b29bf4
Patrick Morrow <git@themorrowgroup.com>
2014-02-28 00:25:50 -0700

    asdf

--- js/controllers/ContactsController.js
+++ js/controllers/ContactsController.js
@@ -33,10 +33,8 @@ wordDuel.controller("ContactsController", function ContactsController($scope, $w
 		body += 'Get the app here: <a href="https://play.google.com/store/apps/details?id=com.mobilewordduel">Word Duel</a><br/>';
 		body += 'Then accept the invitation to play: <a href="https://googledrive.com/host/0B42c8HW7dKbZYUZlZFJaZVU5RlU/transfer.html?deviceRegId=' + deviceRegId + '&inviterName=' + myname + '&inviterEmail=' + myemail +'">Play!</a>';		
 		var torecipients = [contact.emailAddress];
-		$window.plugins.emailComposer.showEmailComposerWithCallback(function() { notify('in callback', 'debug'); }, subject, body, torecipients, null, null, true, null, null);
-	}
-	function emailContactCallback(contact, deviceRegId) {
-		notify('I made it.');
+		$window.plugins.emailComposer.showEmailComposerWithCallback(null, subject, body, torecipients, null, null, true, null, null);
+
 		var players = gamePlayStorage.getPlayerList();
 		players.push({"email":contact.emailAddress,"name":contact.displayName,"deviceRegId":deviceRegId});
 		gamePlayStorage.setPlayerList(players)
